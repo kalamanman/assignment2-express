3ec5704614007ec8d664cb43a9a33a61d4e8271a
kalamanman <34838608+kalamanman@users.noreply.github.com>
2018-11-21 21:14:46 -0500

    Create comments.js

---
+++ routes/comments.js
@@ -0,0 +1,23 @@
+module.exports = {
+    getComments(req, res) {
+       req.postId=int(req.params.postId)
+       res.send(200,(req.posts[postId].comments))
+    }, 
+    addComment(req, res) {
+      postId=int(req.params.postId)
+      commentsId=req.store[postId[comments]].length
+      req.store[postId[commentId]]=req.body 
+     res.send(201,'comment added')
+    },
+    updateComment(req, res) {
+      
+      postId=int(req.params.postId)
+      commentId=int(req.params.commentId)
+      req.store[postId[commentId]]=req.body 
+    },
+    removeComment(req, res){
+      postId=int(req.params.postId)
+      commentId=int(req.params.commentId)
+      req.store[postId[comments]].splice(commentId,1)
+    }  
+}
