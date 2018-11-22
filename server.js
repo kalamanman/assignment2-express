277c99cb89090350fda4984cff75ea274a8fe50c
kalamanman <34838608+kalamanman@users.noreply.github.com>
2018-11-21 21:59:22 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -8,24 +8,12 @@ let store = {}
 let posts=[]
 let comments=[]
 const app=express()
-app.use(logger('dev'))
-    
-    
+app.use(logger('dev')) 
     app.use((req,res,next)=>{
       req.store=store
        req.posts=posts
        console.log(req.url +"\n\n"+req.params +"\n"+req.body)
       next()
-   })
-   app.use((req,res,next)=>{
-       if(req.body&& req.params[postId]){
-         temp={}
-         temp.url=(req.body.url).trim()
-         temp.title=req.body.Title.trim()
-          temp.text=req.body.Text.trim()
-          req.body=JSON.parse(temp)
-          next()
-       }
    })
    app.use(bodyParser.json())
    
@@ -36,14 +24,6 @@ app.use(logger('dev'))
      app.get('/posts:/:postId/comments',routes.comments.getComments)
      app.post('/posts:/:postId/comments',routes.comments.addComment)
      app.put('/posts:/:postId/comments/:commentId',routes.comments.updateComment)
-     app.use(()=>{
-      if(req.body&& req.params[commentId]){
-        temp={}
-       temp.Text=(req.body.Text).trim()
-        req.body=Json.parse(temp)
-
-      }
-     })
      app.delete('/posts:/:postId/comments/:commentId',routes.comments.removeComment)
      
     app.listen(3000)
