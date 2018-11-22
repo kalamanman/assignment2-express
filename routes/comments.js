7161dcbecb40aee517341cd7b76e5efba5aeb00b
kalamanman <34838608+kalamanman@users.noreply.github.com>
2018-11-21 21:47:48 -0500

    Add files via upload

--- routes/comments.js
+++ routes/comments.js
@@ -1,16 +1,16 @@
 module.exports = {
     getComments(req, res) {
        req.postId=int(req.params.postId)
-       res.send(200,(req.posts[postId].comments))
+       res.send(200,(req.store[postId].comments))
     }, 
     addComment(req, res) {
-      postId=int(req.params.postId)
+      postId=int(req.params[postId])
       commentsId=req.store[postId[comments]].length
       req.store[postId[commentId]]=req.body 
+      req.store[postId[comments]].push(req.body)
      res.send(201,'comment added')
     },
     updateComment(req, res) {
-      
       postId=int(req.params.postId)
       commentId=int(req.params.commentId)
       req.store[postId[commentId]]=req.body 
@@ -20,4 +20,5 @@ module.exports = {
       commentId=int(req.params.commentId)
       req.store[postId[comments]].splice(commentId,1)
     }  
-}
+  }
+  
